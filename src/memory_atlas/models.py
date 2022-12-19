fd1d6c7b5d0d81f010ded96ef3637d07f9e6a1c8
James Price <jwprice100@gmail.com>
2022-12-18 22:59:33 -0500

    Fleshing out variable further.

--- src/memory_atlas/models.py
+++ src/memory_atlas/models.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 from dataclasses import dataclass, field
-from . mat_types import BuiltInTypes
+from typing import Union
+from enum import IntEnum
+from . mat_types import BuiltInTypes, Range
+
+class ArrayBoundType(IntEnum):
+    FIXED = 0 
+    VARIABLE = 1
 
 @dataclass
 class SemVer:
@@ -10,18 +16,29 @@ class SemVer:
 
 @dataclass
 class BomVariable:
-    name            : str
-    description     : str
-    var_type        : BuiltInTypes
+    name                : str
+    description         : str
+    var_type            : BuiltInTypes
+    array               : bool = False
+    array_bound_type    : ArrayBoundType = field(default_factory=lambda: ArrayBoundType.FIXED)
+    var_array_bound     : Union[string, None] = None
+    var_array_fixed     : Union[string, None] = None
+    spare               : boolean = False
+    reserved            : boolean = False
+    range_constraint    : Union[Range, None] = None
+
+    def validate(self):
+        """TODO: Validate the BOM variable is valid."""
+        pass
 
 @dataclass
 class BinaryObjectModel:
     name        : str
     description : str = "TBD"
-    version     : SemVer = field(default_factory=SemVer)
+    version     : SemVer = field(default_factory=SemVer(1, 0, 0))
     variables   : list[BomVariable] = field(default_factory=list)
 
 @dataclass 
 class MemoryAtlas:
-    mat_version : SemVer = SemVer(0, 1, 0)
+    mat_version : SemVer = field(default_factory=lambda : SemVer(0, 1, 0))
     boms        : list[BomVariable] = field(default_factory=list)
